7e4e39cf0d0de3b8bab50609b06a2e5529257e9a
James Musselwhite <jmusselwhite@pivotal.io>
2020-05-14 13:07:48 -0700

    Did not realize om CLI came from github. Thought it was PivNet

--- pks-autoscaler-setup.sh
+++ pks-autoscaler-setup.sh
@@ -10,9 +10,10 @@ echo "Make sure you are running this script on the OPSMANAGER..."
 if command -v om >/dev/null 2>&1 ; then
   	echo "OM CLI installed. Skipping...."
 else
-  	echo "ERROR!!!!"
-  	echo "OM CLI not found. Please download the latest binary locally from PIVNET, set the permissions to execute, and move it to /usr/local/bin"
-	echo "Once installed, please re-run this script."
+  	echo "OM CLI not installed. Installing...."
+    wget https://github.com/pivotal-cf/om/releases/download/4.5.0/om-linux-4.5.0
+    chmod +x om-linux-4.5.0
+    sudo mv om-linux-4.5.0 /usr/local/bin/om
 fi
 
 if command -v pks >/dev/null 2>&1 ; then
