61170b341eb2666f1a37cdaff0dd17ed13dcee39
James Musselwhite <jmusselwhite@pivotal.io>
2020-06-10 16:37:04 -0700

    Delete all VMs with unresponsive agent

---
+++ delete-vms.py
@@ -0,0 +1,46 @@
+import json
+import subprocess
+import sys
+from subprocess import PIPE
+
+DEPLOYMENT=sys.argv[1]
+
+def delete_vm(vm_cid):
+    print("deleting {}".format(vm_cid))
+    completed = subprocess.run(
+        ['bosh -d \'{0}\' -n delete-vm {1}'.format(DEPLOYMENT, vm_cid)],
+        stdout=PIPE,
+        stderr=PIPE,
+        shell=True
+    )
+    print(completed.stdout)
+    
+
+completed = subprocess.run(
+    ['bosh -d \'{0}\' vms --json --tty'.format(DEPLOYMENT)],
+    stdout=PIPE,
+    stderr=PIPE,
+    shell=True
+)
+
+raw = completed.stdout
+
+json_string = raw.decode('utf-8')
+
+vm_json = json.loads(json_string)
+
+vms = vm_json['Tables'][0]['Rows']
+
+print(len(vms))
+dead_vms = []
+for vm in vms:
+    if vm['process_state'] == 'unresponsive agent':
+        dead_vms.append(vm['vm_cid'])
+
+print(dead_vms)
+
+for dead_vm in dead_vms:
+    delete_vm(dead_vm)
+
+
+
